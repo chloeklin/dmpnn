e3e9e040c9fa02557bcc48e1334b84be42a56c83
Chloe Lin <u6788552@MacBook-Pro.local>
2025-07-24 12:18:32 +1000

    fix script

--- scripts/train_large.sh
+++ scripts/train_large.sh
@@ -2,7 +2,7 @@
 
 #PBS -q gpuvolta
 #PBS -P um09
-#PBS -l ncpus=412
+#PBS -l ncpus=12
 #PBS -l ngpus=1
 #PBS -l mem=100GB
 #PBS -l walltime=00:10:00
@@ -12,12 +12,9 @@
 #PBS -N dmpnn_large_insulator
 
 
-#python3/3.12.1
-
-
 cd /scratch/um09/hl4138
 module use /g/data/dk92/apps/Modules/modulefiles
-module load python3/3.12.1 cuda/12.0.0 deepspeed/0.15.1 
+module load python3/3.12.1 cuda/12.0.0
 source venvs/dmpnn-venv/bin/activate
 
 cd dmpnn
