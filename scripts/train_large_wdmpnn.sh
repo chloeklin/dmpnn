48888d1fb262b8c6ac64bdd016bde49876d5e77b
Han Ni Lin <hl4138@gadi-login-08.gadi.nci.org.au>
2025-08-11 13:05:27 +1000

    fix

--- scripts/train_large_wdmpnn.sh
+++ scripts/train_large_wdmpnn.sh
@@ -5,7 +5,7 @@
 #PBS -l ncpus=12
 #PBS -l ngpus=1
 #PBS -l mem=100GB
-#PBS -l walltime=00:30:00
+#PBS -l walltime=00:20:00
 #PBS -l storage=scratch/um09+gdata/dk92
 #PBS -l jobfs=100GB
 #PBS -N wdmpnn_insulator
@@ -17,5 +17,5 @@ source venvs/dmpnn-venv/bin/activate
 
 cd dmpnn
 python3 scripts/train_large_wdmpnn.py --dataset_name insulator > output.txt
-# python3 scripts/train_large_wdmpnn.py --dataset_name polyinfo --task_type multi --n_classes 21
+# python3 scripts/train_large_wdmpnn.py --dataset_name polyinfo --task_type multi --n_classes 21 > polyinfo_output.txt
 
