4b61d7ac743257d354f1a4cf52a420fbb59b0770
Han Ni Lin <hl4138@gadi-login-08.gadi.nci.org.au>
2025-08-11 12:29:50 +1000

    error output

--- scripts/train_large.sh
+++ scripts/train_large.sh
@@ -5,10 +5,10 @@
 #PBS -l ncpus=12
 #PBS -l ngpus=1
 #PBS -l mem=100GB
-#PBS -l walltime=30:00:00
+#PBS -l walltime=05:40:00
 #PBS -l storage=scratch/um09+gdata/dk92
 #PBS -l jobfs=100GB
-#PBS -N dmpnn_b3lyp
+#PBS -N dmpnn_camb
 
 cd /scratch/um09/hl4138
 module use /g/data/dk92/apps/Modules/modulefiles
@@ -16,8 +16,9 @@ module load python3/3.12.1 cuda/12.0.0
 source venvs/dmpnn-venv/bin/activate
 
 cd dmpnn
-# python3 scripts/train_large.py --dataset_name insulator
-# python3 scripts/train_large.py --dataset_name polyinfo --task_type multi --n_classes 21
-# python3 scripts/train_large.py --dataset_name htpmd --descriptor_columns Molality Monomer_Molecular_Weight DoP Density
-python3 scripts/train_large.py --dataset_name opv_b3lyp
-# python3 scripts/train_large.py --dataset_name opv_camb3lyp 
+# python3 scripts/train_large.py --dataset_name insulator --incl_rdkit True
+# python3 scripts/train_large.py --dataset_name polyinfo --task_type multi --n_classes 21 
+# python3 scripts/train_large.py --dataset_name htpmd --descriptor_columns Molality Monomer_Molecular_Weight DoP Density --incl_rdkit True
+# python3 scripts/train_large.py --dataset_name opv_b3lyp --incl_rdkit True
+# python3 scripts/train_large.py --dataset_name opv_camb3lyp --incl_rdkit True
+ 
---
+++ scripts/train_large_wdmpnn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#PBS -q gpuvolta
+#PBS -P um09
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=100GB
+#PBS -l walltime=00:30:00
+#PBS -l storage=scratch/um09+gdata/dk92
+#PBS -l jobfs=100GB
+#PBS -N wdmpnn_insulator
+
+cd /scratch/um09/hl4138
+module use /g/data/dk92/apps/Modules/modulefiles
+module load python3/3.12.1 cuda/12.0.0
+source venvs/dmpnn-venv/bin/activate
+
+cd dmpnn
+python3 scripts/train_large_wdmpnn.py --dataset_name insulator > output.txt
+# python3 scripts/train_large_wdmpnn.py --dataset_name polyinfo --task_type multi --n_classes 21
+
