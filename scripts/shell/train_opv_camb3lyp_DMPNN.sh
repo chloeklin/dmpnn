f05179aceae8b7c37d5f341869c0c7e63ebfb306
Han Ni Lin <hl4138@gadi-login-03.gadi.nci.org.au>
2025-09-05 00:51:09 +1000

    fix

--- scripts/shell/train_opv_camb3lyp_DMPNN.sh
+++ scripts/shell/train_opv_camb3lyp_DMPNN.sh
@@ -2,10 +2,10 @@
 
 #PBS -q gpuvolta
 #PBS -P um09
-#PBS -l ncpus=12
-#PBS -l ngpus=1
+#PBS -l ncpus=24
+#PBS -l ngpus=2
 #PBS -l mem=100GB
-#PBS -l walltime=12:00:00
+#PBS -l walltime=25:00:00
 #PBS -l storage=scratch/um09+gdata/dk92
 #PBS -l jobfs=100GB
 #PBS -N DMPNN_opv_camb3lyp_DMPNN
