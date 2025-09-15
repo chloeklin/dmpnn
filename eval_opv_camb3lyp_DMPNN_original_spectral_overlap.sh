2aa3c50e05bb26e27398641996aa377da9fe0eed
Han Ni Lin <hl4138@gadi-login-09.gadi.nci.org.au>
2025-09-16 09:34:41 +1000

    fix

---
+++ eval_opv_camb3lyp_DMPNN_original_spectral_overlap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#PBS -q gpuvolta
+#PBS -P um09
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=100GB
+#PBS -l walltime=25:00:00
+#PBS -l storage=scratch/um09+gdata/dk92
+#PBS -l jobfs=100GB
+#PBS -N eval_DMPNN_opv_camb3lyp_original_spectral_overlap
+
+module use /g/data/dk92/apps/Modules/modulefiles
+module load python3/3.12.1 cuda/12.0.0
+source /home/659/hl4138/dmpnn-venv/bin/activate
+cd /scratch/um09/hl4138/dmpnn/
+
+# Evaluation
+python3 scripts/python/evaluate_model.py --dataset_name opv_camb3lyp --task_type reg --model_name DMPNN --target spectral_overlap
+
