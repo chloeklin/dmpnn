4bb459ba22320461779708b5e7c8b0ba646f729b
Han Ni Lin <hl4138@gadi-login-05.gadi.nci.org.au>
2025-10-16 13:52:35 +1100

    new res

--- scripts/shell/train_opv_camb3lyp_AttentiveFP_all_size1000_lc.sh
+++ scripts/shell/train_opv_camb3lyp_AttentiveFP_all_size1000_lc.sh
@@ -5,7 +5,7 @@
 #PBS -l ncpus=12
 #PBS -l ngpus=1
 #PBS -l mem=100GB
-#PBS -l walltime=10:00:00
+#PBS -l walltime=12:00:00
 #PBS -l storage=scratch/um09+gdata/dk92
 #PBS -l jobfs=100GB
 #PBS -N AttentiveFP_opv_all_size1000_lc
