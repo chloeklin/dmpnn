63829abbb4d99da76a69d081e8bd76e3c0de1aa5
Han Ni Lin <hl4138@gadi-login-03.gadi.nci.org.au>
2025-10-22 13:08:47 +1100

    new res

---
+++ scripts/shell/train_insulator_DMPNN_bandgap_chain_rdkit_size256_lc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#PBS -q gpuvolta
+#PBS -P um09
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=100GB
+#PBS -l walltime=04:00:00
+#PBS -l storage=scratch/um09+gdata/dk92
+#PBS -l jobfs=100GB
+#PBS -N DMPNN_insulator_bandgap_chain_rdkit_size256_lc
+
+module use /g/data/dk92/apps/Modules/modulefiles
+module load python3/3.12.1 cuda/12.0.0
+source /home/659/hl4138/dmpnn-venv/bin/activate
+cd /scratch/um09/hl4138/dmpnn/
+
+
+# DMPNN learning curve training for target: bandgap_chain, train_size: 256
+python3 scripts/python/train_graph.py \
+    --dataset_name insulator \
+    --model_name DMPNN \
+    --target bandgap_chain \
+    --train_size 256 \
+    --save_predictions \
+    --export_embeddings --incl_rdkit
+
+
+##TODO
+
+# Add additional experiments here as needed
+
