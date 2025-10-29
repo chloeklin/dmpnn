7657bef63c18967565853df2736b24dd46d970d9
Han Ni Lin <hl4138@gadi-login-03.gadi.nci.org.au>
2025-10-29 20:21:10 +1100

    new res

--- scripts/shell/train_insulator_DMPNN_DiffPool.sh
+++ scripts/shell/train_insulator_DMPNN_DiffPool.sh
@@ -5,7 +5,7 @@
 #PBS -l ncpus=12
 #PBS -l ngpus=1
 #PBS -l mem=100GB
-#PBS -l walltime=1:40:00
+#PBS -l walltime=7:40:00
 #PBS -l storage=scratch/um09+gdata/dk92
 #PBS -l jobfs=100GB
 #PBS -N DMPNN_DiffPool_insulator_DMPNN_DiffPool
