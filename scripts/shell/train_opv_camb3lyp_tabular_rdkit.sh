6c17fcb4e777045bc957382cfb16ad6cbd90a183
Han Ni Lin <hl4138@gadi-login-07.gadi.nci.org.au>
2025-11-02 13:06:37 +1100

    new res

--- scripts/shell/train_opv_camb3lyp_tabular_rdkit.sh
+++ scripts/shell/train_opv_camb3lyp_tabular_rdkit.sh
@@ -5,10 +5,10 @@
 #PBS -l ncpus=12
 #PBS -l ngpus=1
 #PBS -l mem=100GB
-#PBS -l walltime=0:30:00
+#PBS -l walltime=12:00:00
 #PBS -l storage=scratch/um09+gdata/dk92
 #PBS -l jobfs=100GB
-#PBS -N tabular_insulator_tabular_rdkit_ab
+#PBS -N tabular_opv_camb3lyp_tabular_rdkit
 
 module use /g/data/dk92/apps/Modules/modulefiles
 module load python3/3.12.1 cuda/12.0.0
@@ -17,8 +17,7 @@ cd /scratch/um09/hl4138/dmpnn/
 
 
 # tabular training
-python3 scripts/python/train_tabular.py --dataset_name insulator --incl_rdkit --incl_ab
-
+python3 scripts/python/train_tabular.py --dataset_name opv_camb3lyp --incl_rdkit 
 
 ##TODO
 
