b74a79b2815d529d922ced302eac93c4c3d17f04
Chloe Lin <u6788552@MacBook-Pro.local>
2026-02-18 19:06:07 +0800

    fix

--- scripts/python/utils.py
+++ scripts/python/utils.py
@@ -75,7 +75,7 @@ def add_model_specific_args(parser, model_type):
     """Add model-specific arguments to the base parser."""
     if model_type == "dmpnn":
         parser.add_argument('--model_name', type=str, default="DMPNN", 
-                            choices=["DMPNN", "wDMPNN", "PPG", "DMPNN_DiffPool"],
+                            choices=["DMPNN", "wDMPNN", "PPG", "DMPNN_DiffPool", "GAT", "GATv2", "GIN", "GIN0", "GINE"],
                             help='Name of the model to use')
         parser.add_argument('--batch_size', type=int, default=64,
                             help='Batch size for training and evaluation')
